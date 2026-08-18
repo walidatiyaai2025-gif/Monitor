dc38da1a158ab9ef26d5087a48a69b8e1090fec5
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-08-18 21:09:12 +0300

    fix: wire atomic credential lifecycle in production

--- src/Monitor.Web/Program.cs
+++ src/Monitor.Web/Program.cs
@@ -158,9 +158,9 @@ builder.Services.AddSingleton<IConnectionSecretStore>(provider => new ProtectedF
 builder.Services.AddSingleton<IRuntimeCredentialWriter>(provider => (IRuntimeCredentialWriter)provider.GetRequiredService<IConnectionSecretStore>());
 builder.Services.AddSingleton<ISqlConnectionProbe, SqlConnectionProbe>();
 builder.Services.AddSingleton<IServerConnectionTester, ServerConnectionTester>();
-builder.Services.AddSingleton<CredentialLifecycleService>();
+builder.Services.AddSingleton<AtomicCredentialLifecycleService>();
 builder.Services.AddSingleton<ICredentialLifecycleService>(provider => new WriteAheadAuditedCredentialLifecycleService(
-    provider.GetRequiredService<CredentialLifecycleService>(),
+    provider.GetRequiredService<AtomicCredentialLifecycleService>(),
     provider.GetRequiredService<ServerRegistrationMutationGate>(),
     provider.GetRequiredService<IAuditStore>()));
 builder.Services.AddSingleton<ICredentialReadinessService, CredentialReadinessService>();
